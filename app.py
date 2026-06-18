b20fbf593e66bf700785d944daf294c28ec7bf65
Benya284 <non237181@gmail.com>
2026-06-18 21:22:23 +0300

    Create app school

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/for_student')
+def for_student():
+    return render_template('for_student.html', name= "Benya")
+
 
 
 app.run(debug= True)
