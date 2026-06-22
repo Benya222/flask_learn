b4a222d753da49775249c52eefbed9e39ac952d1
Benya284 <non237181@gmail.com>
2026-06-22 20:08:36 +0300

    add menu.html

--- app.py
+++ app.py
@@ -13,6 +13,16 @@ def index():
     status = start_time <= now <= end_time
     return render_template('index.html', is_open= status)
 
+@app.route('/menu')
+def menu():
+    menu = {
+        "Капучино": 80,
+        "Лате": 85,
+        "Еспресо": 60,
+        "Чізкейк": 120,
+        "Панкейки": 95
+    }
+    return render_template('menu.html', base_menu= menu)
 
 
 app.run(debug= True)
