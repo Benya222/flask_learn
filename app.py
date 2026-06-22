9232a96038585adcd6152f48bd8f5a067680de90
Benya284 <non237181@gmail.com>
2026-06-22 09:47:37 +0300

    add index.html and menu.html

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
 
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    now = datetime.datetime.now().time()
+    start_time = datetime.time(11, 0)
+    end_time = datetime.time(22, 0)
+    status = start_time <= now <= end_time
+    return render_template('index.html', is_open= status)
+
 
 
 app.run(debug= True)
