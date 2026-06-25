d0df06563bb08d399c4aec7e1c282e17a139b1a8
Benya284 <non237181@gmail.com>
2026-06-25 20:38:37 +0300

    finished task

--- app.py
+++ app.py
@@ -1,7 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from datetime import date
 
 
 app = Flask(__name__)
+
+
+@app.route('/', methods= ['GET', 'POST'])
+def index():
+    age, age_days = None, None
+
+    if request.method == 'POST':
+        the_day = request.form.get('age_day')
+        the_mounth = request.form.get('age_mounth') 
+        the_year = request.form.get('age_year') 
+
+        birthday = date(int(the_year), int(the_mounth), int(the_day))
+
+        today = date.today()
+        age = today.year - birthday.year
+
+        if (today.month, today.day) < (birthday.month, birthday.day):
+            age -= 1
+
+        age_days = (today - birthday).days
+
+
+    return render_template('index.html', age = age, age_days = age_days)
+
+
 
 
 app.run(debug= True)
