f79706cc669c16d2bacfde14091497daa4c10324
Benya284 <non237181@gmail.com>
2026-06-25 14:07:16 +0300

    finished my homework

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 
 app = Flask(__name__)
+
+
+
+@app.route('/temperature', methods= ['GET', 'POST'])
+def temperature():
+    user_temp = 0
+    to_fahrenheit = 0
+    if request.method == 'POST':
+        user_temp = request.form.get('temp_converter')
+        to_fahrenheit = float(user_temp) * (9 / 5) + 32  
+
+    return render_template('temperature.html', fahrenheit= to_fahrenheit, temperature= user_temp)
+
 
 
 app.run(debug= True)
