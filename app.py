a03dd8a7fea67c1c25ec059a44cab0776f635ea8
Benya284 <non237181@gmail.com>
2026-06-28 11:48:36 +0300

    end the task

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 
 app = Flask(__name__)
+app.secret_key = '****'
 
-
+gift_list = []
 
 @app.route('/', methods= ['GET', 'POST'])
 @app.route('/index', methods= ['GET', 'POST'])
 def index():
-    gift_list = []
-    to_get = None
+    to_get = '*'
     if request.method == 'POST':
         for_add = request.form.get('add_gift')
         if for_add == '':
-            ...
+            flash('Порожній подарунок не можна додати!')
         else:
             gift_list.append(for_add)
+            flash('Подарунок додано!')
+
+        return redirect(url_for('index'))        
+    
     elif request.method == 'GET':
         search = request.args.get('search_gift')
-        if search in gift_list:
-            to_get = 'OK'
-        else:
-            to_get = 'NOT'
+
+        if search:
+            to_get = search in gift_list
 
 
     return render_template('index.html', gift_list= gift_list, to_get= to_get)
