9eba7dc85ad19f03482f9637dd7eab503da8de8a
Benya284 <non237181@gmail.com>
2026-06-30 11:53:44 +0300

    finished all task

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
 
 
 app = Flask(__name__)
+app.secret_key = "lasldl1*"
 
+def valid_tel(tel): 
+    return  tel.isdigit() and len(tel) == 12 and tel.startswith('380')
 
-def valid_tel(tel):
-    pattern = r'^[380]{9}$' 
-    return  
-
+bid_list = {}
 
 @app.route('/')
 def index():
-
     return render_template('index.html')
 
 
@@ -23,9 +22,26 @@ def register():
         name = request.form.get('name')
         tel_number = request.form.get('phone')
 
-
+        if valid_tel(tel_number):
+            bid_list[name] = tel_number
+            flash("Ваша заявка відправлена!")
+            return redirect(url_for('index'))
+        else:
+            flash("Введіть коректний номер телефону!")
+            return redirect(url_for('register'))
 
     return render_template('register.html')
+
+
+@app.route('/admin', methods = ['GET', 'POST'])
+def admin():
+    if request.method == 'POST':
+        status = request.form.get('status')
+        if status:
+            bid_list.pop(status)
+            flash("Заявка оброблена")
+    
+    return render_template('admin.html', bid_list= bid_list)
 
 
 app.run(debug= True)
