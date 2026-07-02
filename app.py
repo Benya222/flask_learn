cbb10ec3d463bea804b72a41dbf2c913a4e4ff5c
Benya284 <non237181@gmail.com>
2026-07-02 21:17:05 +0300

    first commit > product_manager_base

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 
 app = Flask(__name__)
+app.secret_key = 'lslsdo***'
+
+all_products = {}
+
+@app.route('/', methods= ['GET', 'POST'])
+@app.route('/products', methods= ['GET', 'POST'])
+def products():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        price = request.form.get('price')
+        category = request.form.get('category')
+
+        price = float(price)
+        
+        if name in all_products:
+            flash('Такий товар вже є!')
+        else:
+            all_products[name] = {'price': price,
+                                  'category': category
+                                  }
+        return redirect(url_for('products'))
+
+    return render_template('product.html', all_products= all_products)
 
 
+@app.route('/delete/<name_product>')
+def delete(name_product):
+    all_products.pop(name_product)
+    return redirect('products')
 
 
 app.run(debug= True)
